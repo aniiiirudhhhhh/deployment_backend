c9ae04d013eeb8b93af69717d3ab64224fcef79f
aniiiirudhhhhh <spittala@loyaltymethods.com>
2025-09-15 21:55:13 +0530

    trigger

--- server/controllers/adminAuthController.js
+++ server/controllers/adminAuthController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 // const { revokeToken } = require('../middleware/authMiddleware');
 // generate JWT token (expires in 7 days)
+//hello
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "15m" });
 };
